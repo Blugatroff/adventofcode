f2366994e13739ad302db005b70f6d6ed0f60b66
Blugatroff <51498987+Blugatroff@users.noreply.github.com>
2022-12-03 12:23:11 +0100

    Read puzzle input from file by default

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Exception (SomeException (SomeException), try)
 import Control.Monad (void)
 import Data.Either.Combinators (mapLeft)
 import Data.Function ((&))
@@ -7,9 +8,11 @@ import Data.Functor ((<&>))
 import Data.List (find)
 import Data.Maybe
 import Day (Day (partOne, partTwo), Part, Year (days, name))
+import GHC.IO.Exception (IOException (IOError))
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
+import System.IO.Error (isDoesNotExistError)
 import Text.Read (readEither)
 import Util (readInt)
 import qualified Year2021
@@ -21,9 +24,17 @@ data PartName = PartOne | PartTwo
 newtype DayIndex = DayIndex Int
   deriving (Show)
 
+data InputSource = Stdin | File
+  deriving (Show)
+
 data Args
   = Help
-  | Args !Year !DayIndex !(Maybe PartName)
+  | Args
+      { yearIndex :: !Year,
+        dayIndex :: !DayIndex,
+        partName :: !(Maybe PartName),
+        inputSource :: !InputSource
+      }
   deriving (Show)
 
 validateDay :: [Day] -> Int -> Either String DayIndex
@@ -40,10 +51,11 @@ validateYear yearName = case find (\year -> name year == yearName) years of
 parseArgs :: [String] -> Either String Args
 parseArgs [] = Right Help
 parseArgs ["--help"] = Right Help
+parseArgs ("--stdin" : rest) = parseArgs rest <&> \args -> args {inputSource = Stdin}
 parseArgs [year, day] = do
   year <- readInt year >>= validateYear
   day <- readInt day >>= validateDay (days year)
-  return $ Args year day Nothing
+  return $ Args year day Nothing File
 parseArgs [year, day, part] = do
   year <- readInt year >>= validateYear
   day <- readInt day >>= validateDay (days year)
@@ -53,7 +65,7 @@ parseArgs [year, day, part] = do
     "1" -> Right PartOne
     "2" -> Right PartTwo
     part -> Left $ "unexpected part '" <> part <> "' use either 'one', 'two', '1' or '2'"
-  return $ Args year day $ Just part
+  return $ Args year day (Just part) File
 parseArgs _ = Left "unexpected arguments, use --help"
 
 abort :: String -> IO a
@@ -74,12 +86,26 @@ start :: Args -> IO ()
 start Help = do
   putStrLn "Advent Of Code in Haskell"
   putStrLn ""
-  putStrLn "Usage: ./aoc <year> <day> <part>"
-start (Args year (DayIndex dayIndex) part) = do
+  putStrLn "Usage: ./aoc [--stdin] <year> <day> <part>"
+start (Args year (DayIndex dayIndex) part source) = do
   let day = days year !! dayIndex :: Day
   let solveFn = chooseSolveFunction part day
-  stdin <- getContents
-  case solveFn stdin of
+
+  input <- case source of
+    Stdin -> getContents
+    File -> do
+      let path = "./inputs/" <> show year <> "/" <> show (dayIndex + 1) <> ".txt"
+      result <- try (readFile path) :: IO (Either IOError String)
+      case result of
+        Right contents -> return contents
+        Left error | isDoesNotExistError error -> do
+          putStrLn $ "Attempted to read the puzzle input from \"" <> path <> " but the file does not exist!"
+          exitFailure
+        Left error -> do
+          putStrLn $ "Failed to read the file: \"" <> path <> "\"!"
+          exitFailure
+
+  case solveFn input of
     Left error -> abort $ "Error: " <> error
     Right result -> putStrLn $ "Result: \n" <> result
 
