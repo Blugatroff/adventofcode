b6e3ce49e82d914ab06c9ebdae380d9a44a1b10c
Blugatroff <51498987+Blugatroff@users.noreply.github.com>
2022-12-05 20:06:23 +0100

    2022 Day 5

--- src/Year2022.hs
+++ src/Year2022.hs
@@ -5,13 +5,15 @@ import qualified Year2022.Day1 as Day1
 import qualified Year2022.Day2 as Day2
 import qualified Year2022.Day3 as Day3
 import qualified Year2022.Day4 as Day4
+import qualified Year2022.Day5 as Day5
 
 days :: [Day]
 days =
   [ Day Day1.partOne Day1.partTwo,
     Day Day2.partOne Day2.partTwo,
     Day Day3.partOne Day3.partTwo,
-    Day Day4.partOne Day4.partTwo
+    Day Day4.partOne Day4.partTwo,
+    Day Day5.partOne Day5.partTwo
   ]
 
 year :: Year
---
+++ src/Year2022/Day5.hs
@@ -0,0 +1,84 @@
+module Year2022.Day5 (partOne, partTwo) where
+
+import Data.Char (isSpace)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.List (foldl', transpose)
+import qualified Data.Map as M
+import Util (readInt, replace, split, splitOnce, trim)
+import qualified Util
+
+parseLine :: [Char] -> Either String (Maybe Int)
+parseLine [] = Right Nothing
+parseLine s = Just <$> readInt s
+
+transposeStacks :: [String] -> Either String [String]
+transposeStacks input =
+  transpose input
+    <&> reverse
+    <&> filter (/= ']')
+    <&> filter (/= '[')
+    <&> trim isSpace
+    & filter (not . null)
+    & Right
+
+parseStacks :: [String] -> [Stack]
+parseStacks lines = lines <&> drop 1 <&> reverse
+
+parseMove :: String -> Either String Move
+parseMove line =
+  line
+    & replace "move" ""
+    & replace "from" ""
+    & replace "to" ""
+    & split ' '
+    & filter (not . null)
+    & traverse readInt
+    >>= \case
+      [stack, source, destination] -> Right (Move stack source destination)
+      notThreeInts -> Left $ "Failed to parse move from line: " <> line
+
+parseMoves :: [String] -> Either String [Move]
+parseMoves lines = lines & filter (not . null) & traverse parseMove
+
+data Move = Move
+  { amount :: !Int,
+    source :: !Int,
+    destination :: !Int
+  }
+  deriving (Show)
+
+type Stack = [Char]
+
+parse :: String -> Either String (M.Map Int Stack, [Move])
+parse input = do
+  let lines :: [String] = split '\n' input
+  case splitOnce "" lines of
+    Nothing -> Left "failed to find seperation between stacks and moves"
+    Just (stacks, moves) -> do
+      stacks <- parseStacks <$> transposeStacks stacks
+      moves <- parseMoves moves
+      let stackMap = M.fromList $ zip [1 ..] stacks
+      return (stackMap, moves)
+
+applyMove :: ([Char] -> [Char]) -> M.Map Int Stack -> Move -> M.Map Int Stack
+applyMove reverseOrNot stacks move = case (M.lookup (source move) stacks, M.lookup (destination move) stacks) of
+  (Just srcStack, Just destStack) ->
+    M.insert (destination move) (reverseOrNot (take (amount move) srcStack) ++ destStack) stacks
+      & M.insert (source move) (drop (amount move) srcStack)
+  _ -> stacks
+
+onTop :: M.Map Int Stack -> String
+onTop stacks = M.elems stacks >>= take 1
+
+solvePartOne :: (M.Map Int Stack, [Move]) -> String
+solvePartOne (stacks, moves) = foldl' (applyMove reverse) stacks moves & onTop
+
+solvePartTwo :: (M.Map Int Stack, [Move]) -> String
+solvePartTwo (stacks, moves) = foldl' (applyMove id) stacks moves & onTop
+
+partOne :: String -> Either String String
+partOne input = parse input <&> solvePartOne
+
+partTwo :: String -> Either String String
+partTwo input = parse input <&> solvePartTwo
