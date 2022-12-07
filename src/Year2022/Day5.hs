49b2e80a353a0a9270842f0730a7d2c32fb39b90
Blugatroff <51498987+Blugatroff@users.noreply.github.com>
2022-12-07 20:24:01 +0100

    Faster 2022 Day 5

--- src/Year2022/Day5.hs
+++ src/Year2022/Day5.hs
@@ -1,10 +1,17 @@
 module Year2022.Day5 (partOne, partTwo) where
 
+import Control.Loop
+import Control.Monad (foldM, forM_)
+import Control.Monad.Loops (whileM_)
+import Control.Monad.ST (ST, runST)
 import Data.Char (isSpace)
-import Data.Function ((&))
-import Data.Functor ((<&>))
-import Data.List (foldl', transpose)
-import qualified Data.Map as M
+import Data.Function (on, (&))
+import Data.Functor (($>), (<&>))
+import Data.List (foldl', sortBy, transpose)
+import qualified Data.Map.Strict as M
+import qualified Data.Sequence as Seq
+import qualified Data.Vector.Growable as V
+import Foreign (Storable)
 import Util (readInt, replace, split, splitOnce, trim)
 import qualified Util
 
@@ -49,7 +56,7 @@ parse :: String -> Either String (M.Map Int Stack, [Move])
 parse input = do
   let lines :: [String] = split '\n' input
   case splitOnce "" lines of
-    Nothing -> Left "failed to find seperation between stacks and moves"
+    Nothing -> Left "failed to find separation between stacks and moves"
     Just (stacks, moves) -> do
       moves <- parseMoves moves
       let stackMap = M.fromList $ zip [1 ..] $ parseStacks $ transposeStacks stacks
@@ -65,11 +72,87 @@ applyMove reverseOrNot stacks move = case (M.lookup (source move) stacks, M.look
 onTop :: M.Map Int Stack -> String
 onTop stacks = M.elems stacks >>= take 1
 
+solve :: ([Char] -> [Char]) -> (M.Map Int Stack, [Move]) -> String
+solve reverseOrNot (stacks, moves) = output ++ onTop outputStacks
+  where
+    (_, output, outputStacks) = foldl' folder (0, "", stacks) moves
+
+    numberMoves = length moves
+
+    folder :: (Int, String, M.Map Int [Char]) -> Move -> (Int, String, M.Map Int [Char])
+    folder (i, output, stacks) move = (i + 1, output ++ message, applyMove reverseOrNot stacks move)
+      where
+        message = show i <> "/" <> show numberMoves <> "\n"
+
 solvePartOne :: (M.Map Int Stack, [Move]) -> String
-solvePartOne (stacks, moves) = foldl' (applyMove reverse) stacks moves & onTop
+solvePartOne = solve reverse
+
+type FastStacks state = V.GrowableVector state (V.GrowablePrimitiveVector state Char)
+
+newFastStack :: Storable a => [a] -> ST state (V.GrowablePrimitiveVector state a)
+newFastStack items = do
+  vector <- V.new
+  forM_ (reverse items) $ V.push vector
+  return vector
+
+createFastStacks :: M.Map Int Stack -> ST state (FastStacks state)
+createFastStacks map = do
+  stackVector <- V.new
+  newFastStack [] >>= V.push stackVector
+  forM_ stacks $ \stack ->
+    stack >>= V.push stackVector
+  return stackVector
+  where
+    stacks = M.assocs map & sortBy (compare `on` fst) <&> snd <&> newFastStack
+
+copy :: Storable a => Int -> Int -> Int -> V.GrowablePrimitiveVector state a -> V.GrowablePrimitiveVector state a -> ST state ()
+copy srcIndex dstIndex length src dst =
+  forLoop 0 (< length) (+ 1) $ \i ->
+    V.write dst (dstIndex + i) =<< V.read src (srcIndex + i)
+
+applyMoveFast :: FastStacks state -> Move -> ST state ()
+applyMoveFast stacks move = do
+  srcStack <- V.read stacks (source move)
+  dstStack <- V.read stacks (destination move)
+  dstIndex <- V.length dstStack
+  srcIndex <- V.length srcStack <&> \length -> length - amount move
+  let minDstSize = dstIndex + amount move
+  whileM_ (V.length dstStack <&> (< minDstSize)) $ do
+    V.push dstStack '?'
+  copy srcIndex dstIndex (amount move) srcStack dstStack
+  whileM_ (V.length srcStack <&> (> srcIndex)) $ do
+    V.pop srcStack
+
+vectorToList :: V.GrowableVector state a -> ST state [a]
+vectorToList vector = do
+  length <- V.length vector
+  reverse <$> foldM fold [] [0 .. length - 1]
+  where
+    fold list i = do
+      elem <- V.read vector i
+      return $ elem : list
+
+onTopFast :: FastStacks state -> ST state String
+onTopFast stacks = reverse <$> (vectorToList stacks >>= foldM folder "")
+  where
+    folder output stack = V.length stack >>= (V.read stack . subtract 1) <&> (: output)
+
+solveFast :: FastStacks state -> [Move] -> ST state String
+solveFast stacks moves = do
+  (_, outputStacks) <- foldM folder (0, stacks) moves
+  onTopFast outputStacks
+  where
+    numberMoves = length moves
+
+    folder :: (Int, FastStacks state) -> Move -> ST state (Int, FastStacks state)
+    folder (i, stacks) move = do
+      applyMoveFast stacks move
+      return (i + 1, stacks)
 
 solvePartTwo :: (M.Map Int Stack, [Move]) -> String
-solvePartTwo (stacks, moves) = foldl' (applyMove id) stacks moves & onTop
+solvePartTwo (stacks, moves) = runST $ do
+  fastStacks <- createFastStacks stacks
+  solveFast fastStacks moves
 
 partOne :: String -> Either String String
 partOne input = parse input <&> solvePartOne
